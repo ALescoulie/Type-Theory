1ee5f75946f79402af1f9a68558e171c0770101d
ALescoulie <alia.lescoulie@gmail.com>
2024-08-28 10:11:54 -0700

    add flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    systems.url = "github:nix-systems/default";
+    ghc-wasm-meta.url = "https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz";
+  };
+
+  outputs = {
+    systems,
+    nixpkgs,
+    ghc-wasm-meta,
+    ...
+  } @ inputs: let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f nixpkgs.legacyPackages.${system}
+          
+      );
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.haskellPackages.haskell-language-server
+          pkgs.haskellPackages.hindent
+          pkgs.haskell.compiler.ghc98
+          pkgs.cabal-install
+          pkgs.latex
+          pkgs.texliveSmall
+        ];
+      };
+    });
+  };
+}
