484ff3a35d3deee1d83a6bb58626d93a17153bba
ALescoulie <alia.lescoulie@gmail.com>
2024-08-28 10:53:02 -0700

    set up enviornment

--- flake.nix
+++ flake.nix
@@ -1,20 +1,18 @@
+{
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
-    ghc-wasm-meta.url = "https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz";
   };
 
   outputs = {
     systems,
     nixpkgs,
-    ghc-wasm-meta,
     ...
   } @ inputs: let
     eachSystem = f:
       nixpkgs.lib.genAttrs (import systems) (
         system:
-          f nixpkgs.legacyPackages.${system}
-          
+          f nixpkgs.legacyPackages.${system}         
       );
   in {
     devShells = eachSystem (pkgs: {
@@ -24,7 +22,6 @@
           pkgs.haskellPackages.hindent
           pkgs.haskell.compiler.ghc98
           pkgs.cabal-install
-          pkgs.latex
           pkgs.texliveSmall
         ];
       };
